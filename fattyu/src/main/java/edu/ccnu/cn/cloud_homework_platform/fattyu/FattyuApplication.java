b3acfabc98791c8b8316b37e09f8a421ff168e16
RawSharkill <1229002314@qq.com>
2019-12-31 09:15:11 +0800

    [ADD]\

---
+++ fattyu/src/main/java/edu/ccnu/cn/cloud_homework_platform/fattyu/FattyuApplication.java
@@ -0,0 +1,13 @@
+package edu.ccnu.cn.cloud_homework_platform.fattyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FattyuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FattyuApplication.class, args);
+    }
+
+}
---
+++ fattyu/src/test/java/edu/ccnu/cn/cloud_homework_platform/fattyu/FattyuApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ccnu.cn.cloud_homework_platform.fattyu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FattyuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
